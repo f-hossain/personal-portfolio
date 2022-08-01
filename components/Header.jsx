3559697ebf4d65bec9d9dfa0dea6171368bd6775
f-hossain <55707559+f-hossain@users.noreply.github.com>
2022-08-01 17:59:05 -0400

    updated resume

--- components/Header.jsx
+++ components/Header.jsx
@@ -43,7 +43,7 @@ const Header = () => {
                         <a href="#work" className="p-2 lg:px-4 md:mx-2 text-gray-600 pb-3">experience</a>
                         <a href="#projects" className="p-2 lg:px-4 md:mx-2 text-gray-600 pb-3">projects</a>
                         <a href="#contact" className="p-2 lg:px-4 md:mx-2 text-gray-600 pb-3">contact</a>
-                        <a href='https://drive.google.com/file/d/1YQUvaPqVnJIrC6upuwyzxQmCFfuraykK/view?usp=sharing' target="_blank" rel="noreferrer" className="p-2 lg:px-4 md:mx-2 text-gray-600 pb-3">resume</a>
+                        <a href='https://drive.google.com/file/d/1EK6I7EEFwbmd5a92vPCPt85ZyKcfVs78/view?usp=sharing' target="_blank" rel="noreferrer" className="p-2 lg:px-4 md:mx-2 text-gray-600 pb-3">resume</a>
                     </div>
                 </div>
             </nav>
