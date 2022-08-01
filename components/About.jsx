83719b7d58b0fa5d6445c22dde8df8d96123c010
f-hossain <55707559+f-hossain@users.noreply.github.com>
2022-08-01 00:47:54 -0400

    animashun fix oop

--- components/About.jsx
+++ components/About.jsx
@@ -2,8 +2,6 @@ import React from 'react'
 import Image from 'next/image'
 import about from '../public/about.jpg'
 import { motion, useAnimation } from "framer-motion";
-import { useInView } from "react-intersection-observer";
-import { useEffect } from "react";
 
 const About = () => {
   const popVariant = {
@@ -16,15 +14,6 @@ const About = () => {
     hidden: { opacity: 0, x: 250 },
   }
 
-  const control = useAnimation()
-  const [ref, inView] = useInView()
-
-  useEffect(() => {
-    if (inView) {
-      control.start("visible");
-    } 
-  }, [control, inView]);
-
   return (
     <div id='about' className='flex flex-col justify-content w-full pb-12 px-6 md:px-24'>
         {/*  section title */}
@@ -36,10 +25,10 @@ const About = () => {
 
         {/* content container */}
         <div className='flex flex-col md:flex-row justify-center items-center content-center md:space-x-12 md:divide-x-2 border-solid border-neutral-200 md:pt-9 px-2'>
-            <motion.div ref={ref} variants={popVariant} initial="hidden" animate={control} className='w-96'>
+            <motion.div variants={popVariant} initial="hidden" whileInView="visible" viewport={{ once: true }} className='w-96'>
               <Image src={about} />
             </motion.div>
-            <motion.div ref={ref} variants={slideVariant} initial="hidden" animate={control} className='flex flex-col justify-items-center md:justify-items-start w-3/4'>
+            <motion.div variants={slideVariant} initial="hidden" whileInView="visible" viewport={{ once: true }} className='flex flex-col justify-items-center md:justify-items-start w-3/4'>
               <div className='text-xl md:pl-10 pt-3 md:pt-10 underline underline-offset-8 decoration-slate-200 md:no-underline poppins text-stone-700'>Fariha (fah · ree · uh) :</div>
               <div className='md:pl-10 pt-5 md:pb-10 text-sm'>
               <p className='bg-slate-100 p-2 rounded text-slate-500 '>❝ <i>Previously: </i> a bright eyed girl with too much time on her hands and access to tumblr.com who built custom themes for blogs using html/css. 
